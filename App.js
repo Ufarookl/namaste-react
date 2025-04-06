f52dbd8113826af9dcd63fb016ae392f19fbe771
UmarFarook13 <umarfarook1311@gmail.com>
2025-04-06 16:40:39 +0530

    COmpleted Epi 3

--- App.js
+++ App.js
@@ -1,8 +1,42 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world from Umar"
-);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-root.render(heading);
+//React Element
+const jsxhead = (
+  <h1 id="hed1" className="headingclass">
+    HEllo WORld FRom UMar FArook
+  </h1>
+);
+
+// React functional component
+const Title = () => {
+  return (
+    <h1 id="heading" className="headingclass">
+      {" "}
+      Hello This is a Title component
+    </h1>
+  );
+};
+
+//Or I can write the above component in a single line without return as well
+
+const TitleSingleLine = () => (
+  <h1 id="heading" className="headingclass">
+    {" "}
+    Hello This is a Title component in single line
+  </h1>
+);
+
+//component composition => it means component inside a component
+
+const BodyComponent = () => (
+  <div className="container">
+    {jsxhead}
+    <Title />
+    <TitleSingleLine />
+    <h1 className="headingclass"> This is a content inside the component</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BodyComponent />);
